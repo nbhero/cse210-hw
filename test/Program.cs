302ca731fda1791d040ef92b5ba41fa2f0ed25a6
Bruno Rafael <brunorafael72@outlook.com>
2024-03-02 00:39:14 -0300

    Prep 01 OK

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x; 
+            x = 5;
+            if (x < 10)
+            {
+                Console.WriteLine($"{x} is lesser than 10!");
+            }
+            
+          
+        }
+    }
+}
