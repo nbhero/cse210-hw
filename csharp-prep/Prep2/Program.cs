b2343358b06eb3561d0fbfe55678a309d5571928
Bruno Rafael <brunorafael72@outlook.com>
2024-03-02 15:09:41 -0300

    Prep 02 - OK

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string user_grade = Console.ReadLine();
+        int percentage = int.Parse(user_grade);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            remainder = (percentage / 10)
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Better luck next time!");
+        }
     }
 }
