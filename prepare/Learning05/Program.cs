a9d5fa63d7bd735d0354be2ec3505ac1992ebee9
Bruno Rafael <brunorafael72@outlook.com>
2024-03-29 17:40:27 -0300

    Learning 05 - OK

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square square1 = new Square("red", 3);
+        Rectangle rectangle1 = new Rectangle("blue", 10, 40);
+        Circle circle1 = new Circle("green", 5);
+
+        shapes.Add(square1);
+        shapes.Add(rectangle1);
+        shapes.Add(circle1);
+        
+
+        foreach (Shape item in shapes)
+        {
+            string color = item.GetColor();
+
+            double area = item.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
